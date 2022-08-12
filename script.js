90564e9f84d95b36c9a7900b79d87fadaf9bd1db
Vinicius Santiago <vinicius.santiago997@gmail.com>
2022-08-12 05:31:45 -0300

    Requisito 18 realizado

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 /* Caso o email seja "tryber@teste.com" e a senha seja "123456", será emitido um alerta contendo o texto "Olá, Tryber!"; Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos.";  */
-const loginButton = document.querySelector('button');
-const inputEmail = document.querySelectorAll('input')[0];
-const inputPassword = document.querySelectorAll('input')[1];
-loginButton.addEventListener('click', () => {
-  if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
-    alert('Olá, Tryber!');
+const loginButton = document.querySelector("button");
+const inputEmail = document.querySelectorAll("input")[0];
+const inputPassword = document.querySelectorAll("input")[1];
+loginButton.addEventListener("click", () => {
+  if (
+    inputEmail.value === "tryber@teste.com" &&
+    inputPassword.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
+});
+
+let submitBtn = document.querySelector("#submit-btn");
+let agreementCheck = document.querySelector("#agreement");
+
+agreementCheck.addEventListener("click", () => {
+  submitBtn.toggleAttribute("disabled");
 });
